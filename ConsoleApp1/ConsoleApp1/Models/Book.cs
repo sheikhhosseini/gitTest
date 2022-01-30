d6330184db307a380ee0b305e3a93e120e701af9
Saeed-ssd <saeed779911@gmail.com>
2022-01-30 18:46:44 +0330

    add git ignore and models

---
+++ ConsoleApp1/ConsoleApp1/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Pages { get; set; }
+    }
+}
